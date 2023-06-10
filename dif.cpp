b89abb47fabba097e0efeda697723be042219647
Umer qazi <uqazi844@gmail.com>
2023-06-10 16:14:08 +0500

    adding dif java

---
+++ dif.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+
+    a = 4;
+    b = 3;
+
+    c = a-b;
+    cout << "the difference of: " << a << " and " << b << " is: " << c ;
+
+    return 0;
+}
