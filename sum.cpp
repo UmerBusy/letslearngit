46c40e05d77800a91e7b3989e4104dffe9eb9914
Umer qazi <uqazi844@gmail.com>
2023-06-10 16:16:07 +0500

    added message

---
+++ sum.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+
+    a = 1;
+    b = 2;
+
+    c = a + b;
+
+    cout << "the sum is: "<< c ;
+
+    return 0;
+}
